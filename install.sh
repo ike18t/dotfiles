d1044fe8db73b1217f2b4fe68df0a17573d81819
Isaac Datlof <ike18t@gmail.com>
2015-06-26 14:41:23 -0400

    add .gitconfig and link vimrc for neovim

--- install.sh
+++ install.sh
@@ -3,4 +3,6 @@ ln -s ~/Projects/dotfiles/.aliases ~/.aliases
 ln -s ~/Projects/dotfiles/.zshrc ~/.zshrc
 ln -s ~/Projects/dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/Projects/dotfiles/.vimrc ~/.vimrc
+ln -s ~/Projects/dotfiles/.vimrc ~/.nvimrc # neovim
 ln -s ~/Projects/dotfiles/.vimrc.bundles ~/.vimrc.bundles
+ln -s ~/Projects/dotfiles/.gitconfig ~/.gitconfig
