b9c4f5885564ded51bf8cba0e67a32e050dac7e7
Isaac Datlof <ike18t@gmail.com>
2020-07-21 16:38:32 -0400

    symlink tmux conf to tmate conf

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ ln -s ~/Projects/dotfiles/.init_iam ~/.init_iam
 ln -s ~/Projects/dotfiles/.aliases ~/.aliases
 ln -s ~/Projects/dotfiles/.zshrc ~/.zshrc
 ln -s ~/Projects/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/Projects/dotfiles/.tmux.conf ~/.tmate.conf
 ln -s ~/Projects/dotfiles/.vimrc* ~
 ln -s ~/Projects/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/Projects/dotfiles/.alacritty.yml ~/.alacritty.yml
